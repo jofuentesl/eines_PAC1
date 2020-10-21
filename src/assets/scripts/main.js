7c9503069561f091757be5913cbfdea1bf0a26be
jofuentesl <jofuentesle@uoc.edu>
2020-10-21 12:14:56 +0200

    update menu responsive

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -2,6 +2,8 @@
 import "@fortawesome/fontawesome-free/css/all.css"
 import "./import-jquery";
 
-  $("p").click(function(){
-    $(this).hide();
-  });
+
+$('.nav__icon').click(function(){
+  $('.nav__icon').toggleClass('active');
+  $('.nav__menu').toggleClass('nav__menu--display');
+});
